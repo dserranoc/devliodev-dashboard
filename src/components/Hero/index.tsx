ae252d93cf88a86e35162e7379cef9bce9b37ebb
Daniel Serrano Cobos <d.serranoc@outlook.com>
2023-04-04 13:23:18 +0200

    feat(Root): Add blur effect to Hero section

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -2,13 +2,19 @@ import Link from 'next/link'
 import GithubAuthButton from '../GithubAuthButton'
 export default function Hero () {
   return (
-    <header className='bg-white dark:bg-gray-900 flex flex-auto items-center'>
+    <header className='dark:bg-gray-900 flex flex-auto items-center'>
       <div className='py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16'>
-        <h1 className='mb-4 text-5xl font-extrabold tracking-tight leading-none text-gray-900 md:text-6xl lg:text-7xl dark:text-white'>Es el momento de dar visibilidad a tu <span className='bg-gradient-to-r from-devlio-purple-500 via-devlio-purple-600 to-devlio-purple-700 00 inline-block text-transparent bg-clip-text'>software</span></h1>
+        <div
+          className='-z-10 absolute inset-0 max-w-lg m-auto h-[15rem] sm:h-72 sm:max-w-7xl' style={{
+            background: 'linear-gradient(106.89deg, rgba(192, 132, 252, 0.11) 15.73%, rgba(14, 165, 233, 0.41) 15.74%, rgba(232, 121, 249, 0.26) 56.49%, rgba(79, 70, 229, 0.4) 115.91%)',
+            filter: 'blur(100px)'
+          }}
+        />
+        <h1 className='mb-4 text-5xl font-extrabold tracking-tight leading-none text-gray-900 md:text-6xl lg:text-7xl dark:text-white'>Es el momento de dar visibilidad a tu <span className='z-20 bg-gradient-to-r from-devlio-purple-500 via-devlio-purple-600 to-devlio-purple-700 00 inline-block text-transparent bg-clip-text'>software</span></h1>
         <p className='mb-8 text-xl font-normal text-gray-500 lg:text-2xl sm:px-16 lg:px-48 dark:text-gray-400'>devlio.dev es una plataforma donde puedes mostrar al mundo los proyectos software que hayas realizado.</p>
         <div className='flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4'>
           <GithubAuthButton displayStyle='block sm:hidden' />
-          <Link href='#' className='inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800'>
+          <Link href='#' className='inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 bg-white/50 hover:bg-white focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800'>
             Leer más
           </Link>
         </div>
