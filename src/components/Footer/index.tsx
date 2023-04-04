4e2cb6e8a4d529363e33f2f740433ed66d65218f
Daniel Serrano Cobos <d.serranoc@outlook.com>
2023-04-04 13:27:10 +0200

    fix(Root): Make some minor changes

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ export default function Footer () {
   return (
     <footer className='bg-white dark:bg-gray-900'>
       <div className='mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8'>
-        <hr className='my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8' />
+        <hr className='mb-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:mb-8' />
         <div className='flex items-center justify-between'>
           <span className='text-sm text-gray-500 sm:text-center dark:text-gray-400'>Copyright © {new Date().getFullYear()} <a href='https://devlio.dev' className='hover:underline'>Devliodev</a>
           </span>
