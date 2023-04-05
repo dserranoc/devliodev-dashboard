2c68e3d7e941de854125709a082ac371ededbcbe
Daniel Serrano Cobos <d.serranoc@outlook.com>
2023-04-05 11:03:40 +0200

    refactor(Root): Move width of containers to layout

--- src/components/Layouts/Layout.tsx
+++ src/components/Layouts/Layout.tsx
@@ -3,10 +3,13 @@ import Navbar from '../Navbar'
 
 export default function Layout ({ children }: { children: React.ReactNode }) {
   return (
-    <main className='flex flex-col h-screen'>
-      <Navbar />
-      {children}
-      <Footer />
-    </main>
+    <div className='dark:bg-gray-900'>
+      <main className='flex flex-col h-screen max-w-screen-xl mx-auto'>
+        <Navbar />
+        {children}
+        <Footer />
+      </main>
+    </div>
+
   )
 }
