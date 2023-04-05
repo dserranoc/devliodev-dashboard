52f054a397c7cf0bed5bfbe91319656aa155ae8b
Daniel Serrano Cobos <d.serranoc@outlook.com>
2023-04-05 14:35:48 +0200

    feat(Root): Allow to close dropdown when click outside

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,13 +1,30 @@
 import LogoutButton from '../LogoutButton'
-import { useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 
 export default function Avatar ({ src, alt }: { src: string, alt: string }) {
   const [dropdownOpen, setDropdownOpen] = useState(false)
+
+  const menuDropdownRef = useRef<HTMLDivElement>(null)
+  const avatarRef = useRef<HTMLImageElement>(null)
+
+  useEffect(() => {
+    const checkIfClickedOutside = (e: any) => {
+      if (dropdownOpen && (menuDropdownRef.current != null) && !menuDropdownRef.current.contains(e.target) && avatarRef.current !== null && !avatarRef.current.contains(e.target)) {
+        setDropdownOpen(false)
+      }
+    }
+
+    document.addEventListener('mousedown', checkIfClickedOutside)
+
+    return () => {
+      document.removeEventListener('mousedown', checkIfClickedOutside)
+    }
+  }, [dropdownOpen])
   return (
     <div>
-      <img id='avatarButton' className='w-14 h-14 rounded-full cursor-pointer' src={src} alt={alt} onClick={() => setDropdownOpen(!dropdownOpen)} />
+      <img id='avatarButton' ref={avatarRef} className='w-14 h-14 rounded-full cursor-pointer' src={src} alt={alt} onClick={() => setDropdownOpen(!dropdownOpen)} />
       {/* Dropdown menu */}
-      <div id='userDropdown' className={`z-10 ${!dropdownOpen ? 'hidden' : ''} absolute bg-gray-100 divide-y rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600`}>
+      <div id='userDropdown' ref={menuDropdownRef} className={`z-10 ${!dropdownOpen ? 'hidden' : 'active'} absolute bg-gray-100 divide-y rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600 `}>
         <LogoutButton />
       </div>
     </div>
